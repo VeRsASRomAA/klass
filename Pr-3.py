2d110f346ccf35b3511f100a5150460e5924725b
VeRsASRomAA <157576722+VeRsASRomAA@users.noreply.github.com>
2024-02-28 11:47:10 +0300

    28.02.2024

---
+++ Pr-3.py
@@ -0,0 +1,39 @@
+#1
+p = int(input("Введите число: "))
+i= 2
+while i < p:
+    print(i)
+    i+=1
+#2
+a= int(input("Введите число A: "))
+b= int(input("Введите число B: "))
+i= b-1
+while i > a:
+    print(i)
+    i-=1
+#3
+a= int(input("Введите число A: "))
+b= int(input("Введите число B: "))
+i= b+1
+while i < a:
+    print(i)
+    i+=1
+#5
+n= int(input("Введите число n: "))
+k=0
+while 3**k <=n:
+    k+=1
+    print(k)
+#4
+n= int(input("Введите число n: "))
+k= int(input("Введите число k: "))
+while 3**k >=n:
+    k-=1
+    print(k)
+#6
+n= int(input("Введите число n: "))
+k= int(input("Введите число k: "))
+k=0
+while k < n:
+    k+=1
+    print(k)
