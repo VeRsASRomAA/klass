c48e4a0168424289678181d1a6f0881a74011f0a
VeRsASRomAA <157576722+VeRsASRomAA@users.noreply.github.com>
2024-03-06 11:58:20 +0300

    06.03.2024

---
+++ Pr-4.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+#10
+i = 28
+while i <= 600:
+    if i % 28 == 0:
+        i += 28
+        print(i)
+#11
+b=1
+a=10
+while a < 20:
+    a+= a * 0.1
+    b+= 1
+print(b)
+#12
+n=int(input("Введите число: "))
+while y=(1/2)+(3/4)+((2*n-1)/(2*n)):
+#14
+n=1
+while n * 11 < 100:
+    n * 11
+    print(n)
+    n+=1
